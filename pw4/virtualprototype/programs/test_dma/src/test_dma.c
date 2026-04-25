68c3eebc65feda3ee10f5107aca13e0eb30d2b6e
till <till.beyer21@gmail.com>
2026-04-25 21:02:46 +0200

    finished ex1 software test

--- pw4/virtualprototype/programs/test_dma/src/test_dma.c
+++ pw4/virtualprototype/programs/test_dma/src/test_dma.c
@@ -1,64 +1,58 @@
+/*
+Author : Till Beyer 25.04.2026
+*/
+
 #include <stdint.h>
 #include <stdio.h>
-#include <swap.h>
 
-volatile uint32_t memBuffer[512];
+
+void writeCiMemory(uint32_t address, uint32_t data) { // the ci ID is 0xA5 -> 165 in decimal
+  uint32_t writeOperation= 1<<9;
+  asm volatile("l.nios_rrr r0,%[in1],%[in2],165" ::[in1] "r"(address | writeOperation), [in2]"r"(data));
+}
+
+void readCiMemory(uint32_t address, uint32_t *data) {
+  asm volatile("l.nios_rrr %[out1],%[in1],r0,165" :[out1]"=r"(*data):[in1] "r"(address));
+}
 
 int main() {
-  const uint32_t writeBit = 1<<9;
-  const uint32_t busStartAddress = 1 << 10;
-  const uint32_t memoryStartAddress = 2 << 10;
-  const uint32_t blockSize = 3 << 10;
-  const uint32_t burstSize = 4 << 10;
-  const uint32_t statusControl = 5 << 10;
-  const uint32_t usedCiRamAddress = 50;
-  const uint32_t usedBlocksize = 512;
-  const uint32_t usedBurstSize = 25;
-  uint32_t ramAddress, ramData;
-  printf("Writing\n");
-  for (ramAddress = 0; ramAddress < 512; ramAddress++) {
-    asm volatile("l.nios_rrr r0,%[in1],r0,165" ::[in1] "r"(ramAddress | writeBit)); // we clear the memory
+  uint32_t data;
+
+  printf("\n === Test DMA ===\n");
+
+  printf("Check what's there now:\n");
+  readCiMemory(0, &data);
+  printf("Data at address (000) : %d\n", data);
+  readCiMemory(511, &data);
+  printf("Data at address (1FF) : %d\n", data);
+
+  printf("\n-Overwrite everything with ones\n");
+  for (int i = 0; i < 512; ++i) {
+    writeCiMemory(i, 1);
   }
-  printf("Comparing\n");
-  for (ramAddress = 0; ramAddress < 512; ramAddress++) {
-    asm volatile("l.nios_rrr %[out1],%[in1],r0,165" :[out1]"=r"(ramData):[in1] "r"(ramAddress)); // we control that the memory is empty
-    if (ramData != 0) printf("Error at address 0x%03X : 0x%08X\n", ramAddress, ramData);
+
+  printf("-Check if everything is one\n");
+  for (int i = 0; i < 512; ++i) {
+    readCiMemory(i, &data);
+    if (data != 1) printf("Whoops, at address (%03X) we have '%d' instead of 1\n", i, data);
   }
-  printf("Writing\n");
-  for (ramAddress = 0; ramAddress < 512; ramAddress++) {
-    asm volatile("l.nios_rrr r0,%[in1],%[in2],165" ::[in1] "r"(ramAddress | writeBit), [in2]"r"(ramAddress ^ 0xFFFFFF)); // we fill the memory
+
+  printf("-Write ascending numbers\n");
+  for (int i = 0; i < 25; ++i) {
+    writeCiMemory(i, 2*i);
   }
-  printf("Comparing\n");
-  for (ramAddress = 0; ramAddress < 512; ramAddress++) {
-    asm volatile("l.nios_rrr %[out1],%[in1],r0,165" :[out1]"=r"(ramData):[in1] "r"(ramAddress)); // we control that the memory is correct
-    if (ramData != (ramAddress ^ 0xFFFFFF)) printf("Error at address 0x%03X : 0x%08X\n", ramAddress, ramData);
+
+  printf("-Check if everything is correct\n");
+  for (int i = 0; i < 25; ++i) {
+    readCiMemory(i, &data);
+    if (data != 2*i) printf("Whoops, at address (%03X) we have '%d' instead of %d\n", i, data, 2*i);
   }
-  printf("Emptying mem buffer\n");
-  for (ramAddress = 0; ramAddress < 512; ramAddress++) {
-    memBuffer[ramAddress] = 0;
+
+  printf("-Check if rest is untouched\n");
+  for (int i = 25; i < 512; ++i) {
+    readCiMemory(i, &data);
+    if (data != 1) printf("Whoops, at address (%03X) we have '%d' instead of 1\n", i, data);
   }
-  printf("Initialising DMA registers\n");
-  asm volatile("l.nios_rrr r0,%[in1],%[in2],165" ::[in1] "r"(busStartAddress | writeBit),[in2] "r"((uint32_t) &memBuffer[0]));
-  asm volatile("l.nios_rrr r0,%[in1],%[in2],165" ::[in1] "r"(memoryStartAddress | writeBit),[in2] "r"(usedCiRamAddress));
-  asm volatile("l.nios_rrr r0,%[in1],%[in2],165" ::[in1] "r"(blockSize | writeBit),[in2] "r"(usedBlocksize));
-  asm volatile("l.nios_rrr r0,%[in1],%[in2],165" ::[in1] "r"(burstSize | writeBit),[in2] "r"(usedBurstSize));
-  printf("Performing dma from memory to ciRam\n");
-  asm volatile("l.nios_rrr r0,%[in1],%[in2],165" ::[in1] "r"(statusControl | writeBit),[in2] "r"(1));
-  printf("verifying\n");
-  for (ramAddress = usedCiRamAddress; ramAddress < usedCiRamAddress+usedBlocksize; ramAddress++) {
-    asm volatile("l.nios_rrr %[out1],%[in1],r0,165" :[out1]"=r"(ramData):[in1] "r"(ramAddress&0x1FF)); // we control that the memory is correct
-    if (ramData != 0) printf("Error at address 0x%03X : 0x%08X\n", ramAddress, ramData);
-  }
-  printf("Writing ci memory\n");
-  for (ramAddress = 0; ramAddress < 512; ramAddress++) {
-    asm volatile("l.nios_rrr r0,%[in1],%[in2],165" ::[in1] "r"(ramAddress | writeBit), [in2]"r"(ramAddress ^ 0xFFFFFF00)); // we fill the memory
-  }
-  printf("Performing dma from ciRam to memory\n");
-  asm volatile("l.nios_rrr r0,%[in1],%[in2],165" ::[in1] "r"(statusControl | writeBit),[in2] "r"(2));
-  printf("verifying\n");
-  for (ramAddress = 0; ramAddress < usedBlocksize; ramAddress++) {
-    ramData = swap_u32(memBuffer[ramAddress&0x1FF]);
-    if (ramData != (((ramAddress+usedCiRamAddress)&0x1FF) ^ 0xFFFFFF00) ) printf("Error at address 0x%03X : 0x%08X\n", ramAddress, ramData);
-  }
-  printf("Done\n");
+
+  printf("Test finished\n");
 }
