ff569a3084a8953be3a4518b934c62b2324e329d
seby <sebastien.devaud@epfl.ch>
2026-05-26 13:41:12 +0200

    verilog sobel file

--- project/virtualprototype/systems/singleCore/verilog/or1420SingleCore.v
+++ project/virtualprototype/systems/singleCore/verilog/or1420SingleCore.v
@@ -322,7 +322,7 @@ module or1420SingleCore ( input wire         clock12MHz,
    * Here we instantiate the CPU
    *
    */
-  wire [31:0] s_cpu1CiResult, s_grayResult, s_ramDmaResult;
+  wire [31:0] s_cpu1CiResult, s_grayResult, s_ramDmaResult, s_concatenate1Result, s_concatenate2Result, s_sobelResult;
   wire [31:0] s_cpu1CiDataA, s_cpu1CiDataB, s_camCiResult, s_delayResult;
   wire [7:0]  s_cpu1CiN;
   wire        s_cpu1CiRa, s_cpu1CiRb, s_cpu1CiRc, s_cpu1CiStart, s_cpu1CiCke, s_cpu1CiDone, s_i2cCiDone, s_delayCiDone;
@@ -334,11 +334,11 @@ module or1420SingleCore ( input wire         clock12MHz,
   wire [3:0]  s_cpu1byteEnables;
   wire        s_cpu1DataValid;
   wire [7:0]  s_cpu1BurstSize;
-  wire        s_spm1Irq, s_grayDone, s_profileDone, s_stall;
+  wire        s_spm1Irq, s_grayDone, s_profileDone, s_stall, s_concatenate1Done, s_concatenate2Done, s_sobelDone;
   
-  assign s_cpu1CiDone = s_hdmiDone | s_swapByteDone | s_flashDone | s_cpuFreqDone | s_i2cCiDone | s_delayCiDone | s_camCiDone | s_grayDone | s_profileDone |
+  assign s_cpu1CiDone = s_hdmiDone | s_swapByteDone | s_flashDone | s_cpuFreqDone | s_i2cCiDone | s_delayCiDone | s_camCiDone | s_grayDone | s_profileDone | s_concatenate1Done | s_concatenate2Done | s_sobelDone |
                         s_ramDmaDone;
-  assign s_cpu1CiResult = s_hdmiResult | s_swapByteResult | s_flashResult | s_cpuFreqResult | s_i2cCiResult | s_camCiResult | s_delayResult | s_grayResult |  
+  assign s_cpu1CiResult = s_hdmiResult | s_swapByteResult | s_flashResult | s_cpuFreqResult | s_i2cCiResult | s_camCiResult | s_delayResult | s_grayResult |  s_concatenate1Result | s_concatenate2Result | s_sobelResult |
                           s_profileResult | s_ramDmaResult; 
 
   or1420Top #( .NOP_INSTRUCTION(32'h1500FFFF)) cpu1
@@ -768,5 +768,36 @@ module or1420SingleCore ( input wire         clock12MHz,
                              s_flashDataValid | s_camDataValid | sGpioDataValid | s_ramDmaDataValid;
  assign s_busy             = s_sdramBusy;
  assign s_burstSize        = s_cpu1BurstSize | s_hdmiBurstSize | s_camBurstSize | s_ramDmaBurstSize;
+
+  /*
+   *
+   * Here we define our module for the sobel
+   *
+   */
+
+
+  sobelCompute #(.customInstructionId(8'd30)) sobel
+                  (.start(s_cpu1CiStart),
+                    .valueA(s_cpu1CiDataA),
+                    .valueB(s_cpu1CiDataB),
+                    .iseId(s_cpu1CiN),
+                    .done(s_sobelDone),
+                    .result(s_sobelResult) );
+
+  concatenate_line1xline2 #(.customInstructionId(8'd31)) concatenate1
+                  (.start(s_cpu1CiStart),
+                    .valueA(s_cpu1CiDataA),
+                    .valueB(s_cpu1CiDataB),
+                    .iseId(s_cpu1CiN),
+                    .done(s_concatenate1Done),
+                    .result(s_concatenate1Result) );
+
+  concatenate_line2xline3 #(.customInstructionId(8'd32)) concatenate2
+                  (.start(s_cpu1CiStart),
+                    .valueA(s_cpu1CiDataA),
+                    .valueB(s_cpu1CiDataB),
+                    .iseId(s_cpu1CiN),
+                    .done(s_concatenate2Done),
+                    .result(s_concatenate2Result) );
  
 endmodule
